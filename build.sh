e35486de7a5d3fbe5c9a59484990aee06eb91fc8
Stephan Fudeus <sfudeus@users.noreply.github.com>
2020-07-18 21:19:26 +0200

    Use date for docker tag

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker buildx build --platform linux/amd64 --platform linux/arm/v7 -t sfudeus/homematic_exporter:latest --push .
+docker buildx build --platform linux/amd64 --platform linux/arm/v7 -t sfudeus/homematic_exporter:$(date +%F) --push .
+
