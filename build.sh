2f396c1de7e92c8475366c089e37e02c4ce867c8
Stephan Fudeus <sfudeus@users.noreply.github.com>
2020-10-03 23:36:33 +0200

    Push to latest, too

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker buildx build --platform linux/amd64 --platform linux/arm/v7 -t sfudeus/homematic_exporter:$(date +%F) --push .
-
+REPO=sfudeus/homematic_exporter
+docker buildx build --platform linux/amd64 --platform linux/arm/v7 -t $REPO:"$(date +%F)" -t $REPO:latest --push .
